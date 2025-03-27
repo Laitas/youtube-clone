756787067350493b2d8da1105dc3c1ac5aa41c88
Laitas <61273117+Laitas@users.noreply.github.com>
2025-03-27 14:21:59 +0200

    Add clerk

--- src/components/buttons/auth-button.tsx
+++ src/components/buttons/auth-button.tsx
@@ -1,15 +1,25 @@
-import React from "react";
+"use client";
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 import { Button } from "../ui/button";
 import { UserCircleIcon } from "lucide-react";
 
 export const AuthButton = () => {
   return (
-    <Button
-      variant="outline"
-      className="rounded-full border-blue-500/20 px-4 py-2 text-sm font-medium text-blue-600 shadow-none hover:text-blue-500"
-    >
-      <UserCircleIcon />
-      Sign in
-    </Button>
+    <>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+      <SignedOut>
+        <SignInButton mode="modal">
+          <Button
+            variant="outline"
+            className="rounded-full border-blue-500/20 px-4 py-2 text-sm font-medium text-blue-600 shadow-none hover:text-blue-500"
+          >
+            <UserCircleIcon />
+            Sign in
+          </Button>
+        </SignInButton>
+      </SignedOut>
+    </>
   );
 };

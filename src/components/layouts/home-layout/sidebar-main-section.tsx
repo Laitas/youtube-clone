a53f5b15cd0e5ad397c562a8162edfeeee847416
Laitas <61273117+Laitas@users.noreply.github.com>
2025-03-27 15:10:36 +0200

    Protect routes

--- src/components/layouts/home-layout/sidebar-main-section.tsx
+++ src/components/layouts/home-layout/sidebar-main-section.tsx
@@ -6,6 +6,7 @@ import {
   SidebarMenuButton,
   SidebarMenuItem,
 } from "@/components/ui/sidebar";
+import { useAuth, useClerk } from "@clerk/nextjs";
 import { FlameIcon, HomeIcon, PlaySquareIcon } from "lucide-react";
 import Link from "next/link";
 
@@ -29,13 +30,25 @@ const items = [
 ];
 
 export const SidebarMainSection = () => {
+  const clerk = useClerk();
+  const { isSignedIn } = useAuth();
   return (
     <SidebarGroup>
       <SidebarGroupContent>
         <SidebarMenu>
           {items.map((item) => (
             <SidebarMenuItem key={item.title}>
-              <SidebarMenuButton tooltip={item.title} asChild isActive={false}>
+              <SidebarMenuButton
+                tooltip={item.title}
+                asChild
+                isActive={false}
+                onClick={(e) => {
+                  if (!isSignedIn && item.auth) {
+                    e.preventDefault();
+                    return clerk.openSignIn();
+                  }
+                }}
+              >
                 <Link href={item.href} className="flex items-center gap-4">
                   <item.icon />
                   <span className="text-sm">{item.title}</span>

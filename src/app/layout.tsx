515bb1930fcd5a67e206d2d5ecdc7225a0c9dcec
Laitas <61273117+Laitas@users.noreply.github.com>
2025-03-28 17:51:30 +0200

    TRPC

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { HomeLayout } from "@/components/layouts/home-layout";
 import { ClerkProvider } from "@clerk/nextjs";
+import { TRPCProvider } from "@/trpc/client";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,7 +31,9 @@ export default function RootLayout({
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
-          <HomeLayout>{children}</HomeLayout>
+          <TRPCProvider>
+            <HomeLayout>{children}</HomeLayout>
+          </TRPCProvider>
         </body>
       </html>
     </ClerkProvider>

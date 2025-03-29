7f030bb9d15c58a1ac4f41034daa4ec3c61b1b6f
Laitas <61273117+Laitas@users.noreply.github.com>
2025-03-29 10:37:58 +0200

    Redis/rate limiting

--- src/trpc/init.ts
+++ src/trpc/init.ts
@@ -1,24 +1,83 @@
-import { initTRPC } from "@trpc/server";
+import { db } from "@/db";
+import { users } from "@/db/schema";
+import { auth } from "@clerk/nextjs/server";
+import { initTRPC, TRPCError } from "@trpc/server";
+import { eq } from "drizzle-orm";
 import { cache } from "react";
 import superjson from "superjson";
+import { Redis } from "@upstash/redis";
+import { Ratelimit } from "@upstash/ratelimit";
 
 export const createTRPCContext = cache(async () => {
-  /**
-   * @see: https://trpc.io/docs/server/context
-   */
-  return { userId: "user_123" };
+  const { userId } = await auth();
+  return { clerkUserId: userId };
 });
+
+export type Context = ReturnType<typeof createTRPCContext>;
+
 // Avoid exporting the entire t-object
 // since it's not very descriptive.
 // For instance, the use of a t variable
 // is common in i18n libraries.
-const t = initTRPC.create({
+const t = initTRPC.context<Context>().create({
   /**
    * @see https://trpc.io/docs/server/data-transformers
    */
   transformer: superjson,
 });
 // Base router and procedure helpers
+
+const redis = new Redis({
+  url: process.env.UPSTASH_REDIS_REST_URL,
+  token: process.env.UPSTASH_REDIS_REST_TOKEN,
+});
+
+const ratelimit = new Ratelimit({
+  redis,
+  limiter: Ratelimit.slidingWindow(10, "1s"),
+  analytics: true,
+  prefix: "ratelimit",
+});
+
 export const createTRPCRouter = t.router;
 export const createCallerFactory = t.createCallerFactory;
 export const baseProcedure = t.procedure;
+export const protectedProcedure = t.procedure.use(async function isAuth(opts) {
+  const ctx = await opts.ctx;
+
+  if (!ctx.clerkUserId) {
+    throw new TRPCError({
+      code: "UNAUTHORIZED",
+      message: "You are not authorized to access this route",
+    });
+  }
+
+  const [user] = await db
+    .select()
+    .from(users)
+    .where(eq(users.clerkId, ctx.clerkUserId));
+
+  if (!user) {
+    throw new TRPCError({
+      code: "UNAUTHORIZED",
+      message: "You are not authorized to access this route",
+    });
+  }
+
+  const { success, remaining } = await ratelimit.limit(ctx.clerkUserId);
+
+  if (!success) {
+    throw new TRPCError({
+      code: "TOO_MANY_REQUESTS",
+      message: `You have reached your rate limit. Try again in ${remaining} seconds`,
+    });
+  }
+
+  return opts.next({
+    ...opts,
+    ctx: {
+      ...ctx,
+      user,
+    },
+  });
+});
